8f8d20672b7f514ef9ab2f826279ae36e47ca2df
Steve Lafranca <steve.lafranca@evernym.com>
2018-06-21 16:28:37 -0600

    homework

--- library.py
+++ library.py
@@ -5,11 +5,17 @@ _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
 _integer_pat = _whole_word(r'\d+')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
+# _date_iso8601_pat = _whole_word(r'\d{4}-\d{2}-\d{2}')
+_date_iso8601_pat = _whole_word(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
+# _date_iso8601_pat = _whole_word(r'\d{4}[- /.](((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))')
+_date_pat = _whole_word(r'\d{2} (J[au]n|Feb|Ma[ry]|Apr|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
+
 
 def mixed_ordinals(text):
     '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
     for match in _mixed_ordinal_pat.finditer(text):
         yield('ordinal', match)
+
 
 def integers(text):
     '''Find integers in text. Don't count floating point numbers.'''
@@ -20,6 +26,7 @@ def integers(text):
             continue
         yield ('integer', match)
 
+
 def scan(text, *extractors):
     '''
     Scan text using the specified extractors. Return all hits, where each hit is a
@@ -29,3 +36,16 @@ def scan(text, *extractors):
     for extractor in extractors:
         for item in extractor(text):
             yield item
+
+
+def dates_iso8601(text):
+    '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
+    for match in _date_iso8601_pat.finditer(text):
+        yield('date', match)
+
+
+def date_pattern(text):
+    '''Match patterns that use text for month'''
+    for match in _date_pat.finditer(text):
+        yield('date', match)
+
--- library_test.py
+++ library_test.py
@@ -1,6 +1,7 @@
 import unittest
 import library
 
+
 NUM_CORPUS = '''
 On the 5th of May every year, Mexicans celebrate Cinco de Mayo. This tradition
 began in 1845 (the twenty-second anniversary of the Mexican Revolution), and
@@ -15,7 +16,7 @@ class TestCase(unittest.TestCase):
     # Helper function
     def assert_extract(self, text, extractors, *expected):
         actual = [x[1].group(0) for x in library.scan(text, extractors)]
-        self.assertEquals(str(actual), str([x for x in expected]))
+        self.assertEqual(str(actual), str([x for x in expected]))
 
     # First unit test; prove that if we scan NUM_CORPUS looking for mixed_ordinals,
     # we find "5th" and "1st".
@@ -29,6 +30,45 @@ class TestCase(unittest.TestCase):
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
+
+    #4
+    def test_extract_date(self):
+        self.assert_extract("First date 2015-07-25.", library.dates_iso8601, "2015-07-25")
+
+    #6/7
+    def test_invalid_date(self):
+        self.assert_extract("hello 2017-99-31.", library.dates_iso8601)
+        # 2015-07(<12)-25(<31)
+
+    #8/9
+    def test_date_format(self):
+        self.assert_extract('This is important 25 Jan 2017.', library.date_pattern, '25 Jan 2017')
+
+    #10+
+    # def test_8601_1(self):
+    #     self.assert_extract('The date is 2018-06-22 18:22:19.123.',
+    #                         library.dates_iso8601, "2018-06-22 18:22:19.123")
+
+    # def test_8601_2(self):
+    #     self.assert  2018-06-22T18:22:19.123
+    #
+    # def test_8601_3(self):
+    #     self.assert  2018-06-22 18:22:19.123 MDT
+    #
+    # def test_8601_4(self):
+    #     self.assert  2018-06-22 18:22:19.123-8000
+    #
+    # def test_8601_5(self):
+    #     self.assert  25 Jun, 2017
+    #
+    # def test_8601_6(self):
+    #     self.assert  123,456,789
+    #
+    # def test_8601_7(self):
+    #     self.assert  2018-06-22 18:22:19.123
+    #
+    # def test_8601_8(self):
+    #     self.assert  2018-06-22 18:22:19.123Z
 
 
 if __name__ == '__main__':
